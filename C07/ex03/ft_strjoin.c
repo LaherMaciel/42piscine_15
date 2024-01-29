4afdb7b82766e9d6ba3d43dd9a4b065398825523
Laher Wenceslau Payot Maciel <lawemces@studant.42lisboa.com>
2024-01-29 20:50:04 +0000

    send

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,44 +6,63 @@
 /*   By: lawences <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 14:29:40 by lawences          #+#    #+#             */
-/*   Updated: 2024/01/29 19:20:30 by lawences         ###   ########.fr       */
+/*   Updated: 2024/01/29 20:04:07 by lawences         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	find_sizeof_join(char **strs)
+int	find_sizeof_join(char **strs, char *sep, int size)
 {
 	int	i;
 	int	j;
-	int	size;
+	int	k;
+	int	size_of_join;
 
-	i = 0;
-	while (strs[i])
+	i = -1;
+	while (strs[++i])
 	{
-		j = 0;
-		while (strs[i][j])
-		{
-			size++;
-			j++;
-		}
-		size++;
-		i++;
+		j = -1;
+		while (strs[i][++j])
+			size_of_join++;
+		k = -1;
+		if (i < size)
+			while (sep[++k])
+				size_of_join++;
 	}
-	size++;
-	return (size);
+	size_of_join++;
+	return (size_of_join);
 }
 
-char *ft_strjoin(int size, char **strs, char *sep)
+char	*ft_join(char **strs, char *sep, char *join, int size)
 {
 	int	i;
 	int	j;
+	int	k;
+	int	l;
+
+	i = -1;
+	k = 0;
+	while (strs[++i])
+	{
+		j = -1;
+		while (strs[i][++j])
+			join[k++] = strs[i][j];
+		l = -1;
+		if (i < size)
+			while (sep[++l])
+				join[k++] = sep[l];
+	}
+	join[k] = '\0';
+	return (join);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
 	char	*join;
 
-	join = (char *) malloc(find_sizeof_join(strs) * sizeof(char));
-	i = 0;
-	while (strs[i])
-	{
-		
-	}
+	if (size == 0)
+		return ();
+	join = (char *) malloc(find_sizeof_join(strs, sep, size) * sizeof(char));
+	return (ft_join(strs, sep, join, size));
 }
