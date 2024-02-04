bef62db2d9157feb61d4e1e7882d31d44e083eae
Laher Wenceslau Payot Maciel <lawemces@studant.42lisboa.com>
2024-02-04 03:28:39 +0000

    send

--- Rush01_1/search.c
+++ Rush01_1/search.c
@@ -6,19 +6,50 @@
 /*   By: lawences <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 02:03:16 by lawences          #+#    #+#             */
-/*   Updated: 2024/02/04 03:02:06 by lawences         ###   ########.fr       */
+/*   Updated: 2024/02/04 03:28:18 by lawences         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-int	count_missing(int **array)
+/**
+ * @brief see if the value missing is in that colum
+ * 
+ * @param int **array - the array with all the values
+ * @param int val_missing - the value missing
+ * @param int coluna - the colum ur seaching
+*/
+int	search_last_colum(int **array, int val_missing, int coluna)
 {
-	int	missing;
+	int	linha;
 
-	if (missing == 1)
+	linha = 1;
+	while (linha < 5)
 	{
-		
+		if (array[linha][coluna] == val_missing)
+				return (1);
+		linha++;
 	}
+	return (0);
+}
+
+/**
+ * @brief see if the value missing is in that line
+ * 
+ * @param int **array - the array with all the values
+ * @param int val_missing - the value missing
+ * @param int linha - the line ur seaching
+*/
+int	search_last_line(int **array, int val_missing, int linha)
+{
+	int	coluna;
+
+	coluna = 1;
+	while (coluna < 5)
+	{
+		if (array[linha][coluna] == val_missing)
+				return (1);
+		coluna++;
+	}
+	return (0);
 }
 
 /**
@@ -32,71 +63,86 @@ int	count_missing(int **array)
  * 
  * @return int **array
 */
-int	**fill_search_last_colums(int **array, int val_missing, int coluna)
-{
-	int	linha;
-
-	linha = 1;
-	while (linha < 5)
-	{
-		if (array[linha][coluna] == val_missing)
-				return (1);
-		linha++;
-	}
-	return (0);
-}
-
-int	fill_search_last_lines(int **array, int val_missing, int linha)
-{
-	int	coluna;
-
-	coluna = 1;
-	while (coluna < 5)
-	{
-		if (array[linha][coluna] == val_missing)
-				return (1);
-		coluna++;
-	}
-	return (0);
-}
-
 int	**fill_last(int **array, int val_missing)
 {
 	int	linha_livre;
 	int	coluna_livre;
-	int	linha;
-	int	coluna;
+	int	i;
 
-	linha = 1;
+	i = 1;
 	linha_livre = 0;
-	while (linha < 5)
-	{
-		coluna = 1;
-		while (coluna < 5)
-		{
-			if (fill_search_last_line(array, val_missing, linha) == 0)
-				linha_livre = linha;
-			coluna++;
-		}
-		linha++;
-	}
-	if (linha_livre == 0)
-		return (NULL);
-	coluna = 1;
 	coluna_livre = 0;
-	while (coluna < 5)
+	while (i < 5)
 	{
-		linha = 1;
-		while (linha < 5)
-		{
-			if (fill_search_last_colums(array, val_missing, coluna) == 0)
-				coluna_livre = coluna;
-			linha++;
-		}
-		coluna++;
+		if (search_last_line(array, val_missing, i) == 0)
+				linha_livre = i;
+		if (search_last_colum(array, val_missing, i) == 0)
+				coluna_livre = i;
+		i++;
 	}
-	if (coluna_livre == 0)
+	if (linha_livre == 0 || coluna_livre == 0)
 		return (NULL);
 	array[linha_livre][coluna_livre] = val_missing;
 	return (array);
+}
+
+/**
+ * @brief It counts the number of this the value being searched was found
+ *
+ * @param int   **array - The array that have all the values
+ * @param int   seach_val- the value to be searched
+ *
+ * @return int - returns -2 if more then 1 value is missing, -1 if just one is
+ * missing and 0 if all 4 where found.
+*/
+int	count_vals(int **array, int seach_val)
+{
+	int	found;
+	int	i;
+	int	j;
+
+	i = 1;
+	found = 0;
+	while (i < 5)
+	{
+		j = 1;
+		while (j < 5)
+		{
+			if (array[i][j] == seach_val)
+				found++;
+			j++;
+		}
+		i++;
+	}
+	if (found == 3)
+		return (-1);
+	if (found < 3)
+		return (-2);
+	return (0);
+}
+
+/**
+ * @brief search for all missing values and if just 1 is missing it tries to put
+ * it
+ *
+ * @param int   **array - The array that have all the values
+ *
+ * @return int  missing - the number of values missing (example if the 4's are
+ * missing and the 1's then it will return 2) and return 0 if all positions where filled
+*/
+int	missing_val(int **array)
+{
+	int	i;
+	int	missing;
+
+	i = 1;
+	missing == 0;
+	while (i < 5)
+	{
+		if (count_vals(array, i) == -1)
+			fill_last(array, i);
+		if (count_vals(array, i) == -2)
+			missing++;
+	}
+	return (missing);
 }
