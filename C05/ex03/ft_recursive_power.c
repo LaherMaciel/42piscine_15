0f8008a0dd3e9dad3a42618d48a2ef6eee1e8dd8
Laher Wenceslau Payot Maciel <lawemces@studant.42lisboa.com>
2024-02-09 23:45:01 +0000

    send

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -3,19 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lawences <lawences@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lawences <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 17:00:48 by lawences          #+#    #+#             */
-/*   Updated: 2024/01/28 17:06:11 by lawences         ###   ########.fr       */
+/*   Updated: 2024/02/09 22:24:19 by lawences         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
-	if (nb < 0)
+	if (power < 0)
 		return (0);
-	if (nb == 0 && power == 0)
-		return (1);
 	else if (power == 0)
 		return (1);
 	return (nb * ft_recursive_power(nb, (power - 1)));
--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: lawences <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 11:29:36 by lawences          #+#    #+#             */
-/*   Updated: 2024/01/29 14:29:08 by lawences         ###   ########.fr       */
+/*   Updated: 2024/02/09 21:59:59 by lawences         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_ultimate_range(int **range, int min, int max)
 	}
 	*range = malloc((max - min) * sizeof(int));
 	if (!*range)
-		return (0);
+		return (-1);
 	i = min - 1;
 	j = 0;
 	while (++i < max)
